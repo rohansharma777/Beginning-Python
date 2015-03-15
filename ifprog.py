9e0391294860e52cb1f7953b0a8529645f9cc4b6
ROHAN SHARMA <rohansharma2021@yahoo.com>
2015-03-15 10:42:59 +0530

    A program using 'if'

---
+++ ifprog.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: "))
+if num > 10:
+	print("Number is larger than 10.")
+elif num < 10:
+    print("Nummber is less than 10.")
+else:
+    print("It seems you entered 10!")    	
